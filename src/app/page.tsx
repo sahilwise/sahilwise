bbdaf7dac34d846c8d7c631973af58b03e6627cf
sahilwise <hey@sahilwise.com>
2024-12-06 05:52:01 +0530

    done

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>  
+
+      <main>
+            <h1 className="text-center text-4xl mt-32">Sahilwise</h1>
+      </main>
+
+      <footer>
+        <p className="text-center h-screen mt-12">&copy;sahilwise</p>
+      </footer>
+    </>
+  );
+}
