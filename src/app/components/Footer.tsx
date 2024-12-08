794c38792ed34e9ce8735bbc9ea43939e2f6c5a3
sahilwise <hey@sahilwise.com>
2024-12-08 05:37:56 +0530

    done

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Fragment } from "react";
-import { House} from 'lucide-react';
+import { House } from 'lucide-react';
 
 interface FooterProps {
   homepage?: boolean; // Mark props as optional if they aren't always provided
@@ -13,92 +13,104 @@ export default function Footer({ homepage, productspage, opensourcepage }: Foote
   return (
     <Fragment>
       <div className="flex flex-col sm:rounded-3xl">
-        <div className="backdrop-blur-3xl py-3 px-4 text-sm  border-2 border-neutral-900 bg-black/30 rounded-xl text-neutral-500 p-2 fixed sm:bottom-12 bottom-8 left-1/2 transform -translate-x-1/2 w-72">
+        <div className="backdrop-blur-3xl py-3 px-4 text-sm  border-2 border-[#16161d] bg-black/30 rounded-2xl bo text-neutral-500 p-2 fixed sm:bottom-12 bottom-8 left-1/2 transform -translate-x-1/2 w-72">
           <div className="flex sm:justify-between justify-around items-center">
 
 
-          <Link href={"https://x.com/sahilwise"}>
-              <div className="bg-[#292929] rounded-2xl border-2 border-[#3D3D3D]">
-                <Image
-                  src={"/panda.png"}
-                  width={40}
-                  height={40}
-                  alt="twitter or X"
-                  className="rounded-xl"
-                />
-              </div>
-            </Link>
 
-            
+
+
             <Link href={"/"}>
               <div
-                className={`p-3 ${
-                  homepage
+                className={`p-3 ${homepage
                     ? "bg-gradient-to-br from-[#fffaf9] via-[#a8a8a8] to-[#ffffff]"
                     : "bg-neutral-400 text-[#CCCCCC]"
-                } rounded-xl`}
+                  } rounded-xl`}
               >
-                
-                {/* <svg
+
+                <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className={`${
-                    homepage ? "fill-[#16161d]" : "fill-[#FFF]"
-                  }`}
+                  className={`${homepage ? "fill-[#16161d]" : "fill-[#FFF]"
+                    }`}
                   width="16"
                   height="16"
                   viewBox="0 0 24 24"
                 >
                   <path d="M12 6.453l9 8.375v9.172h-6v-6h-6v6h-6v-9.172l9-8.375zm12 5.695l-12-11.148-12 11.133 1.361 1.465 10.639-9.868 10.639 9.883 1.361-1.465z" />
-                </svg> */}
+                </svg>
 
-                <House size={18} color='#000' fill="#000" />
               </div>
             </Link>
 
             <Link href={"/products"}>
               <div
-                className={`p-3 ${
-                  productspage
+                className={`p-3 ${productspage
                     ? "bg-gradient-to-b from-[#cbdb3d] via-[#f4fc1b] to-[#fff4a5]"
                     : "bg-gradient-to-br from-[#312e2d] via-[#2c2b2b] to-[#272525] text-white"
-                } rounded-xl`}
+                  } rounded-xl`}
               >
                 <svg
                   width="16"
                   height="16"
+                  viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
-                  className={`${
-                    productspage ? "fill-[#16161d]" : "fill-[#FFF]"
-                  }`}
+                  className={`${productspage ? "fill-[#16161d]" : "fill-[#FFF]"
+                    }`}
                   fillRule="evenodd"
                   clipRule="evenodd"
                 >
-                  <path d="M11.499 12.03v11.971l-10.5-5.603v-11.835l10.5 5.467zm11.501 6.368l-10.501 5.602v-11.968l10.501-5.404v11.77zm-16.889-15.186l10.609 5.524-4.719 2.428-10.473-5.453 4.583-2.499zm16.362 2.563l-4.664 2.4-10.641-5.54 4.831-2.635 10.474 5.775z" />
-                </svg>
+<path d="M10 16v2.5c0 2.483-2.017 4.5-4.5 4.5-2.484 0-4.5-2.017-4.5-4.5 0-2.484 2.016-4.5 4.5-4.5h2.5v-4h-2.5c-2.484 0-4.5-2.016-4.5-4.5 0-2.483 2.016-4.5 4.5-4.5 2.483 0 4.5 2.017 4.5 4.5v2.5h4v-2.5c0-2.483 2.017-4.5 4.5-4.5 2.484 0 4.5 2.017 4.5 4.5 0 2.484-2.016 4.5-4.5 4.5h-2.5v4h2.5c2.484 0 4.5 2.016 4.5 4.5 0 2.483-2.016 4.5-4.5 4.5-2.483 0-4.5-2.017-4.5-4.5v-2.5h-4zm-2 0h-2.5c-1.379 0-2.5 1.122-2.5 2.5s1.121 2.5 2.5 2.5 2.5-1.122 2.5-2.5v-2.5zm10.5 0h-2.5v2.5c0 1.378 1.121 2.5 2.5 2.5s2.5-1.122 2.5-2.5-1.121-2.5-2.5-2.5zm-4.5-6h-4v4.132h4v-4.132zm-6-2v-2.5c0-1.378-1.121-2.5-2.5-2.5s-2.5 1.122-2.5 2.5 1.121 2.5 2.5 2.5h2.5zm10.5 0c1.379 0 2.5-1.122 2.5-2.5s-1.121-2.5-2.5-2.5-2.5 1.122-2.5 2.5v2.5h2.5z"/>
+</svg>
               </div>
             </Link>
 
             <Link href={"/opensource"}>
               <div
-                className={`p-3 ${
-                  opensourcepage
+                className={`p-3 ${opensourcepage
                     ? "bg-gradient-to-b from-[#cbdb3d] via-[#f4fc1b] to-[#fff4a5]"
                     : "bg-gradient-to-br from-[#312e2d] via-[#2c2b2b] to-[#272525] text-white"
-                } rounded-xl`}
+                  } rounded-xl`}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
-                  className={`${
-                    opensourcepage ? "fill-[#16161d]" : "fill-[#FFF]"
-                  }`}
+                  className={`${opensourcepage ? "fill-[#16161d]" : "fill-[#FFF]"
+                    }`}
                   viewBox="0 0 24 24"
                 >
-                  <path d="M23 0l-15.996 3.585v13.04c-2.979-.589-6.004 1.671-6.004 4.154 0 2.137 1.671 3.221 3.485 3.221 2.155 0 4.512-1.528 4.515-4.638v-10.9l12-2.459v8.624c-2.975-.587-6 1.664-6 4.141 0 2.143 1.715 3.232 3.521 3.232 2.14 0 4.476-1.526 4.479-4.636v-17.364z" />
+<path d="m21.301 4c.411 0 .699.313.699.663 0 .248-.145.515-.497.702-1.788.948-3.858 4.226-3.858 6.248 3.016-.092 4.326 2.582 4.326 4.258 0 2.007-1.738 4.129-4.308 4.129-3.24 0-4.83-2.547-4.83-5.307 0-5.98 6.834-10.693 8.468-10.693zm-10.833 0c.41 0 .699.313.699.663 0 .248-.145.515-.497.702-1.788.948-3.858 4.226-3.858 6.248 3.016-.092 4.326 2.582 4.326 4.258 0 2.007-1.739 4.129-4.308 4.129-3.241 0-4.83-2.547-4.83-5.307 0-5.98 6.833-10.693 8.468-10.693z" fillRule="nonzero"/>
                 </svg>
               </div>
             </Link>
+
+
+            <Link href={"/opensource"}>
+              <div
+                className={`p-3 ${opensourcepage
+                    ? "bg-gradient-to-b from-[#cbdb3d] via-[#f4fc1b] to-[#fff4a5]"
+                    : "bg-gradient-to-br from-[#312e2d] via-[#2c2b2b] to-[#272525] text-white"
+                  } rounded-xl`}
+              >
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  className={`${opensourcepage ? "fill-[#16161d]" : "fill-[#FFF]"
+                    }`}
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M0 1v16.981h4v5.019l7-5.019h13v-16.981h-24zm13 12h-8v-1h8v1zm6-3h-14v-1h14v1zm0-3h-14v-1h14v1z" />
+                </svg>
+
+
+              </div>
+            </Link>
+
+
+
+
 
 
           </div>
